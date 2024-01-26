b4e245a295cd535718f47b5a71ec3a3f7df3fcb5
nmmn4947 <nmmn4947@gmail.com>
2024-01-26 21:41:26 +0700

    some_testing_things

---
+++ BEEG_TURKEY/Assets/Navigation_Code/Navigation.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navigation : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float range;
+    [SerializeField] float maxDistance;
+}
