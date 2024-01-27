b9a82d887e7f7c6dd1d28cb8c0a85ceda16e834a
nmmn4947 <nmmn4947@gmail.com>
2024-01-27 21:26:03 +0700

    Stuff

--- BEEG_TURKEY/Assets/Script/Random_Baby/Car.cs
+++ BEEG_TURKEY/Assets/Script/Random_Baby/Car.cs
@@ -2,42 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Car : MonoBehaviour
+public class Car : Things
 {
-    public bool WANT = false;
-    [SerializeField] private float happyTime;
-    public float sadTime;
-    public float sadTimeKeep;
-    private float hapTimeKeep;
 
-    KidWant_Operate kwo;
-    private void Start()
-    {
-        kwo = GetComponentInParent<KidWant_Operate>();
-        sadTimeKeep = sadTime;
-        hapTimeKeep = happyTime;
-    }
-    void Update()
-    {
-        if (WANT)
-        {
-
-            //if baby got da Doll
-            hapTimeKeep -= Time.deltaTime;
-            if (hapTimeKeep <= 0)
-            {
-                kwo.Done = true;
-                WANT = false;
-                hapTimeKeep = happyTime;
-            }
-            //else baby don't get da Doll
-            sadTimeKeep -= Time.deltaTime;
-            if (sadTimeKeep <= 0)
-            {
-                kwo.Done = true;
-                WANT = false;
-                sadTimeKeep = sadTime;
-            }
-        }
-    }
 }
