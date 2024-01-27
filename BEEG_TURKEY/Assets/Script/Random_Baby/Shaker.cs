7257604ebc5d15e0fbdb4a374c4d22402f4e661f
nmmn4947 <nmmn4947@gmail.com>
2024-01-27 21:25:32 +0700

    Inherit

--- BEEG_TURKEY/Assets/Script/Random_Baby/Shaker.cs
+++ BEEG_TURKEY/Assets/Script/Random_Baby/Shaker.cs
@@ -2,49 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shaker : MonoBehaviour
+public class Shaker : Things
 {
-    public bool WANT = false;
-    [SerializeField] private float happyTime;
-    public float sadTime;
-    public float sadTimeKeep;
-    private float hapTimeKeep;
-
-    KidWant_Operate kwo;
-    private void Start()
-    {
-        kwo = GetComponentInParent<KidWant_Operate>();
-        sadTimeKeep = sadTime;
-        hapTimeKeep = happyTime;
-    }
-    void Update()
-    {
-        if (WANT)
-        {
-
-            if (Input.GetKey(KeyCode.E)) {
-                hapTimeKeep -= Time.deltaTime;
-                if (hapTimeKeep <= 0)
-                {
-                    kwo.Done = true;
-                    WANT = false;
-                    hapTimeKeep = happyTime;
-                }
-            }
-            else if(Input.GetKey(KeyCode.Q))
-            {
-                sadTimeKeep -= Time.deltaTime;
-                if (sadTimeKeep <= 0)
-                {
-                    kwo.Done = true;
-                    WANT = false;
-                    sadTimeKeep = sadTime;
-                }
-            }
-
-        }
-
-
-
-    }
+    
 }
